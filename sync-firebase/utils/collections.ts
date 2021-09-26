aae6e24041a693674a6900b62e3cba5b3a70c5ae
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2021-09-26 16:27:48 +0200

    implemented storing user NFTs from Moralis request

---
+++ sync-firebase/utils/collections.ts
@@ -0,0 +1,339 @@
+export default {
+    user: 'User',
+    userSubcollections: {
+        followers: 'Followers',
+        followings: 'Followings',
+        notifications: 'Notifications',
+        streamingsWatchlist: 'StreamingsWatchlist',
+    },
+    cronHistory: 'CronHistory',
+    waitListEthAddress: 'WaitListEthAddress',
+    userLikes: 'UserLikes',
+    userMedia: 'UserMedia',
+    userFeed: 'UserFeed',
+    userFeedAudios: 'UserFeedAudios',
+    userNFT: 'UserNFT',
+    owned: 'Owned',
+    collectionIds: 'collectionIds',
+    NftMasterCollectionTest: 'NftMasterCollectionTest',
+    UserNFTTest: 'UserNFTTest',
+
+    sythethicFractionalised: 'sytheticFractionalised',
+
+    artworkLiked: 'ArtworkLiked',
+
+    tokens: 'Tokens',
+    wallet: 'Wallet',
+    walletNFT: 'WalletNFT',
+    walletFT: 'WalletFT',
+    points: 'Points',
+
+    // token types
+    unknown: 'UNKNOWN',
+    cryptoToken: 'CRYPTO',
+    nftToken: 'NFTPOD',
+    ftToken: 'FTPOD',
+    socialToken: 'SOCIAL',
+    communityToken: 'COMMUNITY',
+    badgeToken: 'BADGE',
+
+    rates: 'ratesOfChange',
+
+    registeredEthAddress: 'RegisteredEthAddress',
+    catalog: 'catalog',
+    // balance histories
+    balanceHistory: 'BalanceHistory',
+    historyCrypto: 'HistoryCRYPTO',
+    historySocial: 'HistorySOCIAL',
+    historyFT: 'HistoryFTPOD',
+    historyNFT: 'HistoryNFTPOD',
+    historyMusicMedia: 'HistoryMusicMedia',
+    historyVideoMedia: 'HistoryVideoMedia',
+    historyDigitalArtMedia: 'HistoryDigitalArtMedia',
+    historyWritingMedia: 'HistoryWritingMedia',
+
+    priviScan: 'PriviScan',
+    transactions: 'Transactions',
+    transfer: 'Transfer',
+    history: 'History',
+    allTransactions: 'AllTransaction',
+    ethTransactions: 'EthTransactions',
+    waxTransactions: 'WaxTransactions',
+    waitlist: 'Waitlist',
+    cloudDatabase: 'CloudDatabase',
+
+    // privi credit
+    priviCredits: 'PriviCredits',
+    priviCreditsLending: 'PriviCreditsLending',
+    priviCreditsBorrowing: 'PriviCreditsBorrowing',
+    priviCreditsTransactions: 'PriviCreditsTransactions',
+    priviCreditInterestHistory: 'PriviCreditsInterestHistory',
+    priviCreditDepositedHistory: 'PriviCreditsDepositedHistory',
+    priviCreditBorrowedHistory: 'PriviCreditsBorrowedHistory',
+    priviCreditAvailableHistory: 'PriviCreditsAvailableHistory',
+    trendingPriviCredit: 'TrendingPriviCredit',
+
+    // community
+    communityProposal: 'CommunityProposal',
+    community: 'Community',
+    trendingCommunity: 'TrendingCommunity',
+    communityLP: 'CommunityLP',
+    votation: 'Votation',
+    votationState: 'VotationState',
+    voter: 'Voter',
+    voting: 'Voting',
+    ad: 'Ad',
+    proposalHistoryMap: {
+        CommunityCreation: '',
+        CommunityTokenCreation: '',
+        CommunityAirdrop: 'AirdropProposalHistory',
+        CommunityAllocation: 'AllocationProposalHistory',
+        CommunityTransfer: 'TransferProposalHistory',
+        CommunityTreasurer: 'TreasurerProposalHistory',
+        CommunityEjectTreasurer: 'TreasurerProposalHistory',
+        CommunityJoiningRequest: 'MemberProposalHistory',
+        CommunityEjectMember: 'MemberProposalHistory',
+        CommunityBid: 'AcquisitionProposalHistory',
+        MemberCommunityBid: 'AcquisitionProposalHistory',
+        CommunityBuyOrder: 'AcquisitionProposalHistory',
+        CommunityBuying: 'AcquisitionProposalHistory',
+        CommunityExchange: 'AcquisitionProposalHistory',
+    },
+
+    // pods
+    podDiscussionMessage: 'PodDiscussionMessage',
+    podDiscussions: 'PodDiscussions',
+    podsFT: 'PodsFT',
+    trendingPodsFT: 'TrendingPodsFT',
+    buyingOffers: 'BuyingOffers',
+    sellingOffers: 'SellingOffers',
+    podsNFT: 'PodsNFT',
+    trendingPodsNFT: 'TrendingPodsNFT',
+    podTransactions: 'PodTransactions',
+    priceOfTheDay: 'PriceOfTheDay',
+    priceHistory: 'PriceHistory',
+    supplyHistory: 'SupplyHistory',
+    podRoleInvitation: 'PodRoleInvitation',
+    mediaPods: 'MediaPods',
+    trendingMusicMediaPods: 'TrendingMusicMediaPods',
+    musicMediaPods: 'MusicMediaPods',
+    trendingMediaPods: 'TrendingMediaPods',
+    medias: 'Medias',
+    communityMarketings: 'CommunityMarketings',
+    podPolls: 'PodPolls',
+
+    podViewInvitation: 'PodViewInvitation',
+    insurancePools: 'InsurancePools',
+    insuranceClients: 'InsuranceCleints',
+    insuranceInvestors: 'InsurnaceInvestors',
+    notificaction: 'Notification',
+
+    constants: 'manualConstants',
+    traditionalLendingConstants: 'TraditionalLendingConstants',
+    podFTConstants: 'FTPodConstants',
+    podNFTConstants: 'NFTPodConstants',
+    reserveConstants: 'StakingReserveContants',
+
+    // liquidity pools
+    liquidityPools: 'LiquidityPools',
+    liquidityHistory: 'LiquidityHistory',
+    rewardHistory: 'RewardHistory',
+    liquidityPairs: 'LiquidityPairs',
+    feeHistory: 'FeeHistory',
+    traxBettingGame: 'TraxBettingGame',
+
+    // staking
+    stakingRates: 'StakingRates',
+    stakingDeposit: 'StakingDeposit',
+    userStakings: 'UserStakings',
+    stakingToken: 'StakingToken',
+    retunHistory: 'ReturnHistory',
+    stakedHistory: 'StakedHistory',
+
+    badgesHistory: 'BadgesHistory',
+    badges: 'Badges',
+    issues: 'Issues',
+    proposals: 'Proposals',
+    campaigns: 'Campaigns',
+
+    forumCategory: 'ForumCategory',
+    forumPost: 'ForumPost',
+    forumComment: 'ForumComment',
+    project: 'Project',
+    blogPost: 'BlogPost',
+
+    wallPost: 'WallPost',
+
+    podWallPost: 'PodWallPost',
+    podNFTWallPost: 'PodNFTWallPost',
+    creditWallPost: 'CreditWallPost',
+    insuranceWallPost: 'InsuranceWallPost',
+    communityWallPost: 'CommunityWallPost',
+    userWallPost: 'UserWallPost',
+
+    communityDiscussion: 'CommunityDiscussion',
+
+    insurance: 'Insurance',
+
+    chat: 'Chat',
+    message: 'Message',
+    discordChat: 'DiscordChat',
+    discordRoom: 'DiscordRoom',
+    discordMessage: 'DiscordMessage',
+    discordMessageReplies: 'DiscordMessageReplies',
+
+    levels: 'Levels',
+
+    socialPools: 'SocialPools',
+    perks: 'Perks',
+
+    pendingNotifications: 'PendingNotifications',
+
+    tasks: 'Tasks',
+
+    workInProgress: 'WorkInProgress',
+
+    growthPredictions: 'GrowthPredictions',
+
+    collabs: 'Collabs',
+
+    shareStreaming: 'ShareStreaming',
+    sharePlaylist: 'SharePlaylist',
+
+    // MEDIAS
+    ethMedia: 'EthMedia', // all external media
+    mediaUsers: 'EthMediaUsers',
+    waxMedia: 'EthMediaWax',
+    waxMediaCollections: 'EthMediaWaxCollections',
+    zoraMedia: 'EthMediaZora',
+    openseaMedia: 'EthMediaOpensea',
+    openseaMediaCollections: 'EthMediaOpenseaCollections',
+    mirrorMedia: 'EthMediaMirror',
+    foundationMedia: 'EthMediaFoundation',
+    topshotMedia: 'EthMediaTopshot',
+    sorareMedia: 'EthMediaSorare',
+    shottimeMedia: 'EthMediaShowtime',
+    showtimeMediaCollections: 'EthMediaShowtimeCollections',
+    hicetnuncMedia: 'EthMediaHicetnunc',
+    binanceMedia: 'EthMediaBinance',
+    binanceUsers: 'EthUsersBinance',
+    openseaUsers: 'EthUsersOpensea',
+    hicetnuncUsers: 'EthUsersHicetnunc',
+    foundationUsers: 'EthUsersFoundation',
+    mirrorUsers: 'EthUsersMirror',
+    showtimeUsers: 'EthUsersShowtime',
+    sorareUsers: 'EthUsersSorare',
+    waxUsers: 'EthUsersWax',
+    zoraUsers: 'EthUsersZora',
+    topshotUsers: 'EthUsersTopshot',
+    fractionPriceHistory: 'FractionPriceHistory',
+    fractionOwnershipHistory: 'FractionOwnershipHistory',
+    mediaOnCommunity: 'MediaOnCommunity',
+    bidHistory: 'BidHistory',
+    withdrawals: 'Withdrawals',
+    repayments: 'Repayments',
+    mediaOnCommunityChat: 'MediaOnCommunityChat',
+    mediaOnCommunityMessage: 'MediaOnCommunityMessage',
+    mediaDigitalArt: 'MediaDigitalArt',
+
+    // Fractionalized Vault
+    fractionalizedNFT: 'FractionalizedNFT',
+    syntheticCollections: 'SyntheticCollections',
+    syntheticNFT: 'SyntheticNFT',
+
+    streaming: 'Streaming',
+    streamingLiveVideo: 'StreamingLiveVideo',
+    streamingLiveAudio: 'StreamingLiveAudio',
+    streamingSubcollections: {
+        meetingTokens: 'MeetingTokens',
+        streamerEarnings: 'StreamerEarnings',
+        watchlistedByUsers: 'WatchlistedByUsers',
+
+        listeners: 'Listeners',
+        listenersSubcollections: {
+            blockchainStreamings: 'BlockchainStreamings',
+        },
+    },
+
+    mediaFraction: 'MediaFraction', // Fractionalise
+    ownershipHistory: 'OwnershipHistory',
+    recommendedMedia: 'RecommendedMedia',
+    playList: 'PlayList',
+
+    marketingMediaCommunityChat: 'MarketingMediaCommunityChat',
+    marketingMediaCommunityMessage: 'MarketingMediaCommunityMessage',
+
+    waxActions: 'WaxActions',
+
+    //Exchange
+    exchange: 'Exchange',
+    offers: 'Offers',
+    // NFT Loan
+    nftLoan: 'NFTLoan',
+
+    // Claimable pod
+    claimableSongs: 'ClaimableSongs',
+    claimableSongAlbums: 'ClaimableSongAlbums',
+    claimableSongArtists: 'ClaimableSongArtists',
+    claimableSongGenres: 'ClaimableSongGenres',
+    claimableSongsArtistClaim: 'ArtistClaim',
+    claimableSongsDistributionProposal: 'DistributionProposal',
+    claimableSongsMessage: 'claimableSongsMessage',
+    trendingClaimablePods: 'TrendingClaimablePods',
+    claimableSongsChat: 'ClaimableSongsChat',
+    claimableSongsChatMessage: 'ClaimableSongsChatMessage',
+    claimableSongsSearch: 'ClaimableSongsSearch',
+    claimableSongsListening: 'ClaimableSongsListening',
+    claimableSongsBopProviders: 'BopProviders',
+    claimableSongsTokens: 'Tokens',
+
+    // Stories
+    storyDetail: 'ArtworkStoryDetail',
+    storyMeta: 'ArtworkStoryMeta',
+    storyWatchHistory: 'ArtworkStoryWatchHistory',
+
+    // House
+    UnityHouse: 'UnityHouse',
+    UnityHouseComment: 'UnityHouseComment',
+    UnityAvatar: 'UnityAvatar',
+    UnityCore: 'UnityCore',
+    UnityLeaderboards: 'UnityLeaderboards',
+
+    musicDaoDiscussions: 'MusicDaoDiscussions',
+    musicDaoDiscussionsReply: 'MusicDaoDiscussionsReply',
+    musicDaoTrendingSongs: 'MusicDaoTrendingSongs',
+    musicDaoTopArtist: 'MusicDaoTopArtist',
+    musicDaoProposals: 'MusicDaoProposals',
+    musicDaoProposalsReply: 'MusicDaoProposalsReply',
+    musicDaoProposalsVotes: 'MusicDaoProposalsVotes',
+    musicDaoPolls: 'MusicDaoPolls',
+    musicDaoPollsReply: 'MusicDaoPollsReply',
+    musicDaoPollsVotes: 'MusicDaoPollsVotes',
+
+    // Privi Data
+    priviDataDiscussion: 'PriviDataDiscussion',
+    likes: 'Likes',
+    songsReproduction: 'SongsReproduction',
+
+    // tracking daily/hourly
+    trackingMusicPlayed: 'TrackingMusicPlayed',
+    trackingMusicUploaded: 'TrackingMusicUploaded',
+    trackingLiquidityAmount: 'TrackingLiquidityAmount',
+    trackingStackedAmount: 'TrackingStackedAmount',
+    collectionDataCount: 'CollectionDataCount',
+
+    reference: 'Reference',
+
+    matic: 'Matic',
+
+    // Privi App
+    priviApps: 'PriviApps',
+
+    // Top sellers
+    topSellers: 'TopSellers',
+
+    idoClaims: 'IDOClaims',
+
+    postTRAX: 'PostTRAX',
+    postPIX: 'PostPIX',
+} as const;
